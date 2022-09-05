f4399e2ec0d1873e0a6474793ae0e590158c84d7
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-05 10:09:51 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World");
+
+        System.out.print("Hello\n");
+        System.out.println("World");
+
+        System.out.println("Hello\nWorld");
+        // \n alt satıra iner.
+
+        System.out.println("Hello\tWorld");
+        // \t bir tab kadar boşluk oluşturur.
     }
 }
